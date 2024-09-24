d30021a7c1f7f3651018f9048200e699153a57b2
Marco Vizgarra <marcovizgarra@gmail.com>
2024-09-24 00:37:22 -0300

    Handlebars y Socket.io instalados

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
+import { Server } from 'socket.io'
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import express from 'express';
 
 const app = express();
+const httpServer = app.listen(8080, () => {
+    console.log("Listening on port 8080");
+})
+const socketServer = new Server(httpServer);
+
+// Configurando motor de planillas de handlebars
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname, '/views');
+app.set('view engine', 'handlebars');
 
 import productRouter from './routes/products.router.js';
 import cartRouter from './routes/cart.router.js';
-
-app.listen(8080, () => {
-    console.log("Listening on port 8080");
-})
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
